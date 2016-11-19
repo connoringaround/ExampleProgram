e6b72d56d45e396a4fb878cc0d61775ac2f1245b
connoringaround <connorleerivera@gmail.com>
2016-11-19 13:16:00 -0600

    Added Project

---
+++ Playground1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Playground1.MvcApplication" Language="C#" %>
---
+++ Playground1/Models/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Playground1.Models
+{
+    public class Class1
+    {
+
+        public String Slide { get; set; }
+
+  
+    }
+}
